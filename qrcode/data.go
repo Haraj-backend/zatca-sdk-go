b75944c84532f04d629e25cebf9495dbed857d52
Riandy Rahman Nugraha <riandyrn@gmail.com>
2021-12-03 19:40:57 +0300

    refactor: make Data.Validate() testable, rename SellerTRN into SellerTaxNumber, fix typo in setValue()

--- qrcode/data.go
+++ qrcode/data.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"strconv"
 	"time"
@@ -14,42 +15,54 @@ import (
 // every field values is 255.
 const maxValueLength = 255
 
+var (
+	ErrMissingSellerName      = errors.New("missing `SellerName`")
+	ErrMissingSellerTaxNumber = errors.New("missing `SellerTaxNumber`")
+	ErrMissingTimestamp       = errors.New("missing `Timestamp`")
+	ErrMissingInvoiceTotal    = errors.New("missing `InvoiceTotal`")
+	ErrMissingTotalVAT        = errors.New("missing `TotalVAT`")
+
+	ErrSellerNameTooLong      = errors.New("`SellerName` is too long")
+	ErrSellerTaxNumberTooLong = errors.New("`SellerTaxNumber` is too long")
+)
+
 // Data represents decoded data inside QRCode
 type Data struct {
-	SellerName   string
-	SellerTRN    string
-	Timestamp    time.Time
-	InvoiceTotal float64
-	TotalVAT     float64
+	SellerName      string
+	SellerTaxNumber string
+	Timestamp       time.Time
+	InvoiceTotal    float64
+	TotalVAT        float64
 }
 
 // Validate is used for validating Data
 func (i Data) Validate() error {
 	if len(i.SellerName) == 0 {
-		return fmt.Errorf("missing `SellerName`")
+		return ErrMissingSellerName
 	}
-	if len(i.SellerTRN) == 0 {
-		return fmt.Errorf("missing `SellerTRN`")
+	if len(i.SellerTaxNumber) == 0 {
+		return ErrMissingSellerTaxNumber
 	}
 	if i.Timestamp == (time.Time{}) {
-		return fmt.Errorf("missing `Timestamp`")
+		return ErrMissingTimestamp
 	}
 	if i.InvoiceTotal == 0 {
-		return fmt.Errorf("missing `InvoiceTotal`")
+		return ErrMissingInvoiceTotal
 	}
 	if i.TotalVAT == 0 {
-		return fmt.Errorf("missing `TotalVAT")
+		return ErrMissingTotalVAT
 	}
 	strMap := map[string]string{
-		"SellerName":   i.SellerName,
-		"SellerTRN":    i.SellerTRN,
-		"Timestamp":    formatTime(i.Timestamp),
-		"InvoiceTotal": formatFloat(i.InvoiceTotal),
-		"TotalVAT":     formatFloat(i.TotalVAT),
+		"SellerName":      i.SellerName,
+		"SellerTaxNumber": i.SellerTaxNumber,
+	}
+	errMap := map[string]error{
+		"SellerName":      ErrSellerNameTooLong,
+		"SellerTaxNumber": ErrSellerTaxNumberTooLong,
 	}
 	for fieldName, str := range strMap {
-		if len([]byte(str)) > maxValueLength {
-			return fmt.Errorf("`%v` exceeding max value length", fieldName)
+		if len(str) > maxValueLength {
+			return errMap[fieldName]
 		}
 	}
 	return nil
@@ -72,7 +85,7 @@ func EncodeTLV(d Data) (string, error) {
 	// construct hash
 	buf := new(bytes.Buffer)
 	buf.Write(encodeValue(1, d.SellerName))
-	buf.Write(encodeValue(2, d.SellerTRN))
+	buf.Write(encodeValue(2, d.SellerTaxNumber))
 	buf.Write(encodeValue(3, formatTime(d.Timestamp)))
 	buf.Write(encodeValue(4, formatFloat(d.InvoiceTotal)))
 	buf.Write(encodeValue(5, formatFloat(d.TotalVAT)))
@@ -115,21 +128,21 @@ func setValue(idx int, val string, d *Data) error {
 	case 1:
 		d.SellerName = val
 	case 2:
-		d.SellerTRN = val
+		d.SellerTaxNumber = val
 	case 3:
 		d.Timestamp, err = time.Parse(time.RFC3339, val)
 		if err != nil {
-			return fmt.Errorf("timestamp format shoud be in RFC3339")
+			return fmt.Errorf("timestamp format should be in RFC3339")
 		}
 	case 4:
 		d.InvoiceTotal, err = strconv.ParseFloat(val, 64)
 		if err != nil {
-			return fmt.Errorf("invoice total format shoud be float number")
+			return fmt.Errorf("invoice total format should be float number")
 		}
 	case 5:
 		d.TotalVAT, err = strconv.ParseFloat(val, 64)
 		if err != nil {
-			return fmt.Errorf("total VAT format shoud be float number")
+			return fmt.Errorf("total VAT format should be float number")
 		}
 	}
 	return nil
--- qrcode/data_test.go
+++ qrcode/data_test.go
@@ -7,7 +7,110 @@ import (
 	"github.com/Haraj-backend/zatca-sdk-go/qrcode"
 )
 
-func TestValidate(t *testing.T) {}
+func TestValidate(t *testing.T) {
+	testCases := []struct {
+		Name     string
+		Data     qrcode.Data
+		ExpError error
+	}{
+		{
+			Name: "Test Valid Data - No Arabic",
+			Data: qrcode.Data{
+				SellerName:      "Bobs Records",
+				SellerTaxNumber: "310122393500003",
+				Timestamp:       time.Date(2022, 04, 25, 15, 30, 00, 00, time.UTC),
+				InvoiceTotal:    1000,
+				TotalVAT:        150,
+			},
+			ExpError: nil,
+		},
+		{
+			Name: "Test Valid Data - With Arabic",
+			Data: qrcode.Data{
+				SellerName:      "الجواهري العربي",
+				SellerTaxNumber: "310122393500003",
+				Timestamp:       time.Date(2022, 04, 25, 15, 30, 00, 00, time.UTC),
+				InvoiceTotal:    1000,
+				TotalVAT:        150,
+			},
+			ExpError: nil,
+		},
+		{
+			Name: "Test Invalid Data - Long Seller Name Alphabet",
+			Data: qrcode.Data{
+				SellerName:      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies sem quis enim pellentesque auctor. In sit amet posuere erat, non aliquam ligula. Vestibulum pretium quis metus vel blandit. Maecenas nec molestie tellus, eget efficitur nunc. Nulla ullamcorper quis nibh eu pretium. Donec et nulla urna. Fusce sapien dolor, consectetur non lorem et, ultrices rhoncus ante. In sed imperdiet mi, sed faucibus massa. Nulla facilisi. Donec molestie eros et metus eleifend faucibus. Nulla scelerisque ex sed turpis.",
+				SellerTaxNumber: "310122393500003",
+				Timestamp:       time.Date(2022, 04, 25, 15, 30, 00, 00, time.UTC),
+				InvoiceTotal:    1000,
+				TotalVAT:        150,
+			},
+			ExpError: qrcode.ErrSellerNameTooLong,
+		},
+		{
+			Name: "Test Invalid Data - Long Seller Name Arabic",
+			Data: qrcode.Data{
+				SellerName:      "هنالك العديد من الأنواع المتوفرة لنصوص لوريم إيبسوم، ولكن الغالبية تم تعديلها بشكل ما عبر إدخال بعض النوادر أو الكلمات العشوائية إلى النص. إن كنت تريد أن تستخدم نص لوريم إيبسوم ما، عليك أن تتحقق أولاً أن ليس هناك أي كلمات أو عبارات محرجة أو غير لائقة مخبأة في هذا النص. بينما تعمل جميع مولّدات نصوص لوريم إيبسوم على الإنترنت على إعادة تكرار مقاطع من نص لوريم إيبسوم نفسه عدة مرات بما تتطلبه الحاجة، يقوم مولّدنا هذا باستخدام كلمات من قاموس يحوي على أكثر من 200 كلمة لا تينية، مضاف إليها مجموعة من الجمل النموذجية، لتكوين نص لوريم إيبسوم ذو شكل منطقي قريب إلى النص الحقيقي. وبالتالي يكون النص الناتح خالي من التكرار، أو أي كلمات أو عبارات غير لائقة أو ما شابه. وهذا ما يجعله أول مولّد نص لوريم إيبسوم حقيقي على الإنترنت.",
+				SellerTaxNumber: "310122393500003",
+				Timestamp:       time.Date(2022, 04, 25, 15, 30, 00, 00, time.UTC),
+				InvoiceTotal:    1000,
+				TotalVAT:        150,
+			},
+			ExpError: qrcode.ErrSellerNameTooLong,
+		},
+		{
+			Name: "Test Invalid Data - Missing Seller Name",
+			Data: qrcode.Data{
+				SellerName:      "",
+				SellerTaxNumber: "310122393500003",
+				Timestamp:       time.Date(2022, 04, 25, 15, 30, 00, 00, time.UTC),
+				InvoiceTotal:    1000,
+				TotalVAT:        150,
+			},
+			ExpError: qrcode.ErrMissingSellerName,
+		},
+		{
+			Name: "Test Invalid Data - Missing Seller Tax Number",
+			Data: qrcode.Data{
+				SellerName:      "Bobs Records",
+				SellerTaxNumber: "",
+				Timestamp:       time.Date(2022, 04, 25, 15, 30, 00, 00, time.UTC),
+				InvoiceTotal:    1000,
+				TotalVAT:        150,
+			},
+			ExpError: qrcode.ErrMissingSellerTaxNumber,
+		},
+		{
+			Name: "Test Invalid Data - Missing Invoice Total",
+			Data: qrcode.Data{
+				SellerName:      "Bobs Records",
+				SellerTaxNumber: "310122393500003",
+				Timestamp:       time.Date(2022, 04, 25, 15, 30, 00, 00, time.UTC),
+				InvoiceTotal:    0,
+				TotalVAT:        150,
+			},
+			ExpError: qrcode.ErrMissingInvoiceTotal,
+		},
+		{
+			Name: "Test Invalid Data - Missing Total VAT",
+			Data: qrcode.Data{
+				SellerName:      "Bobs Records",
+				SellerTaxNumber: "310122393500003",
+				Timestamp:       time.Date(2022, 04, 25, 15, 30, 00, 00, time.UTC),
+				InvoiceTotal:    1000,
+				TotalVAT:        0,
+			},
+			ExpError: qrcode.ErrMissingTotalVAT,
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			err := testCase.Data.Validate()
+			if err != testCase.ExpError {
+				t.Errorf("mismatch error, exp: %v, got: %v", testCase.ExpError, err)
+			}
+		})
+	}
+}
 
 func TestEncodeTLV(t *testing.T) {
 	testCases := []struct {
@@ -18,22 +121,22 @@ func TestEncodeTLV(t *testing.T) {
 		{
 			Name: "Test No Arabic Name",
 			Data: qrcode.Data{
-				SellerName:   "Bobs Records",
-				SellerTRN:    "310122393500003",
-				Timestamp:    time.Date(2022, 04, 25, 15, 30, 00, 00, time.UTC),
-				InvoiceTotal: 1000,
-				TotalVAT:     150,
+				SellerName:      "Bobs Records",
+				SellerTaxNumber: "310122393500003",
+				Timestamp:       time.Date(2022, 04, 25, 15, 30, 00, 00, time.UTC),
+				InvoiceTotal:    1000,
+				TotalVAT:        150,
 			},
 			ExpHash: "AQxCb2JzIFJlY29yZHMCDzMxMDEyMjM5MzUwMDAwMwMUMjAyMi0wNC0yNVQxNTozMDowMFoEBzEwMDAuMDAFBjE1MC4wMA==",
 		},
 		{
 			Name: "Test Arabic Name",
 			Data: qrcode.Data{
-				SellerName:   "الجواهري العربي",
-				SellerTRN:    "310122393500003",
-				Timestamp:    time.Date(2022, 04, 25, 15, 30, 00, 00, time.UTC),
-				InvoiceTotal: 1000,
-				TotalVAT:     150,
+				SellerName:      "الجواهري العربي",
+				SellerTaxNumber: "310122393500003",
+				Timestamp:       time.Date(2022, 04, 25, 15, 30, 00, 00, time.UTC),
+				InvoiceTotal:    1000,
+				TotalVAT:        150,
 			},
 			ExpHash: "AR3Yp9mE2KzZiNin2YfYsdmKINin2YTYudix2KjZigIPMzEwMTIyMzkzNTAwMDAzAxQyMDIyLTA0LTI1VDE1OjMwOjAwWgQHMTAwMC4wMAUGMTUwLjAw",
 		},
@@ -61,22 +164,22 @@ func TestDecodeTLV(t *testing.T) {
 			Name:      "Test No Arabic Name",
 			HashInput: "AQxCb2JzIFJlY29yZHMCDzMxMDEyMjM5MzUwMDAwMwMUMjAyMi0wNC0yNVQxNTozMDowMFoEBzEwMDAuMDAFBjE1MC4wMA==",
 			ExpData: qrcode.Data{
-				SellerName:   "Bobs Records",
-				SellerTRN:    "310122393500003",
-				Timestamp:    time.Date(2022, 04, 25, 15, 30, 00, 00, time.UTC),
-				InvoiceTotal: 1000,
-				TotalVAT:     150,
+				SellerName:      "Bobs Records",
+				SellerTaxNumber: "310122393500003",
+				Timestamp:       time.Date(2022, 04, 25, 15, 30, 00, 00, time.UTC),
+				InvoiceTotal:    1000,
+				TotalVAT:        150,
 			},
 		},
 		{
 			Name:      "Test Arabic Name",
 			HashInput: "AR3Yp9mE2KzZiNin2YfYsdmKINin2YTYudix2KjZigIPMzEwMTIyMzkzNTAwMDAzAxQyMDIyLTA0LTI1VDE1OjMwOjAwWgQHMTAwMC4wMAUGMTUwLjAw",
 			ExpData: qrcode.Data{
-				SellerName:   "الجواهري العربي",
-				SellerTRN:    "310122393500003",
-				Timestamp:    time.Date(2022, 04, 25, 15, 30, 00, 00, time.UTC),
-				InvoiceTotal: 1000,
-				TotalVAT:     150,
+				SellerName:      "الجواهري العربي",
+				SellerTaxNumber: "310122393500003",
+				Timestamp:       time.Date(2022, 04, 25, 15, 30, 00, 00, time.UTC),
+				InvoiceTotal:    1000,
+				TotalVAT:        150,
 			},
 		},
 	}
